bb1f024d13f4983c0d9d224b6d76a9222d52d52e
keyjia <13164952317@126.com>
2016-08-16 11:25:14 +0800

    test

--- Flask.py
+++ Flask.py
@@ -1,10 +1,11 @@
 #-*- encoding:utf-8 -*-
-
 from flask import Flask,render_template,request,redirect,url_for,make_response,abort
 from werkzeug.routing import BaseConverter
 from os import path
-#from werkzeug.utils import secure_filename
-
+from werkzeug.utils import secure_filename
+from flask_bootstrap import Bootstrap
+from flask_nav import Nav
+from flask_nav.elements import *
 
 class RegexConverter(BaseConverter):
     def __init__(self,url_map,*items):
@@ -13,7 +14,15 @@ class RegexConverter(BaseConverter):
 
 app = Flask(__name__)
 app.url_map.converters['regex'] = RegexConverter
+Bootstrap(app)
+nav = Nav()
 
+nav.register_element('top',Navbar(u'Flask入门',
+                                  (u'主页','index'),
+                                  (u'关于','about'),
+                                  (u'服务','services'),
+                                  (u'项目','project'),
+                                  ))
 @app.route('/')
 def index():
    response = make_response(render_template('index.html',title='welcome'))
@@ -57,7 +66,6 @@ def upload():
         f.save(upload_path,secure_filename(f.filename))
         return redirect(url_for('upload'))
     return render_template('upload.html')
-
 
 @app.errorhandler(404)
 def page_not_found(error):
